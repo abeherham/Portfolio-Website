b5cb3ec74038f44e9edab2c3b0313bbf08ca07bf
Abraham Hernandez <abeherham@knights.ucf.edu>
2019-08-07 05:35:24 -0400

    Made site more mobile friendly

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import {Link} from 'gatsby'
 const Header = () => {
     return (
         <header>
-            <h4 class="nav-item" id="home"><Link to="/">Abraham Hernandez</Link></h4>
+            <h4 class="nav-item"><Link to="/">Abraham Hernandez</Link></h4>
             <p class="nav-item"><Link to="/aboutMe">About</Link></p>
             <p class="nav-item"><Link to="/projects">Projects</Link></p>
             <p class="nav-item"><Link to="/contact">Contact</Link></p>
